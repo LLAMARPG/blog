0390762a3682cb26397023c7f46348e063fd90a3
Richard Folea <Folea3@gmail.com>
2023-01-10 22:12:38 -0500

    update image pathing for gh

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+//let assetPrefix = '/'
+let basePath = ''
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  //assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
+  //assetPrefix: assetPrefix,
+  basePath: basePath,
+}
+
+module.exports = nextConfig
