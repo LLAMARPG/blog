c786c6e2460dc2302ec637c482e9256db3e42c9e
Richard Folea <Folea3@gmail.com>
2023-02-07 23:13:35 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 //let assetPrefix = '/'
 let basePath = ''
 
-if (isGithubActions) {
+/* if (isGithubActions) {
   // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  //const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   //assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+  basePath = `/`
+} */
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
